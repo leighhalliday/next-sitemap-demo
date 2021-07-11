778d3c2c27b7a66652fe6932948a71b3f0e09fd5
Leigh Halliday <leighhalliday@gmail.com>
2021-07-11 16:20:28 -0400

    Adding links on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Next Sitemap Demo</h1>
+      <ul>
+        <li>
+          <Link href="/capsules">
+            <a>Capsules</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/secret">
+            <a>Secret</a>
+          </Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
